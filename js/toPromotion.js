294de0e9a9cff044996cd5ef17a252937013248b
duml <Lili@dhcp-107-144.palace-wlan.simmons.edu>
2015-09-26 14:37:17 -0400

    changed variable for winsToPromo
    whoops Target doesn’t do what we thought

--- js/toPromotion.js
+++ js/toPromotion.js
@@ -1,4 +1,5 @@
-function toPromotion(winRate, winsToPromo) {
+function toPromotion(winRate currentLP) {
+  var winsToPromo = Math.ceil((100 - currentLP) / 20);
   var lossRate = 1 - winRate;
   Math.ceil((winsToPromo / winRate) + (.5 * (winsToPromo * lossRate)));
 }
