cf978d7360dc76046ebd5f1d20f5c39d6889ac8f
teresakrause <krause.t@husky.neu.edu>
2015-09-26 15:18:59 -0400

    nasty copy pasta

--- js/riot_api.js
+++ js/riot_api.js
@@ -1,9 +1,9 @@
 var API_KEY = "b157e107-4466-49cd-bc5e-8370450e66e0";
 var REQUEST_URL_SHORT = 'https://na.api.pvp.net/api/lol/na';
 
-function onSubmit(summonerName, callback) {
+function onSubmit(summonerName) {
   if (summonerName) {
-    getSummonerId(summonerName, getSummonerStats)
+    getSummonerId(summonerName, function(id) { getLeaguePoints(id, getSummonerStats) })
   }
 }
 
@@ -14,24 +14,15 @@ function getSummonerId(name, callback) {
     dataType: 'json',
     data: {
     },
-    success: function (json) {
-      callback();
+    success: function (data) {
+      var sum_id = data[name.replace(" ", "").toLowerCase().trim()].id;
+      var sum_level = data[name.replace(" ", "").toLowerCase().trim()].summonerLevel;
 
-      var SUMMONER_NAME_NOSPACES = name.replace(" ", "");
+      $('summonerData').attr('name', name);
+      $('summonerData').attr('id', sum_id);
+      $('summonerData').attr('level', sum_level);
 
-      SUMMONER_NAME_NOSPACES = SUMMONER_NAME_NOSPACES.toLowerCase().trim();
-
-      summonerLevel = json[SUMMONER_NAME_NOSPACES].summonerLevel;
-      summonerID = json[SUMMONER_NAME_NOSPACES].id;
-
-      document.getElementById("sLevel").innerHTML = summonerLevel;
-      document.getElementById("sID").innerHTML = summonerID;
-
-      // give sumName the Summoner Name from the json Object
-      sumName = json[SUMMONER_NAME_NOSPACES].name;
-      
-      sumID = summonerID;
-      callback(summonerID);
+      callback(sum_id);
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
       alert("error getting Summoner data!");
@@ -39,34 +30,61 @@ function getSummonerId(name, callback) {
   });
 }
 
-function getSummonerStats(summonerID) {
+function getLeaguePoints(id, callback) {
   $.ajax({
-    url: REQUEST_URL_SHORT + '/v1.3/stats/by-summoner/' + sumID + '/ranked' + '?api_key=' + API_KEY,
+  url: 'https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/' + id + '?api_key=' + API_KEY,
+  type: 'GET',
+  dataType: 'json',
+  data: {
+  },
+  success: function (data) {
+    var entries = data[id][0]['entries']
+    var  lp = 0;
+    for (var i = 0; i < entries.length; i++) {
+      if (entries[i]['playerOrTeamId'] == id) {
+        lp = entries[i]['leaguePoints'];
+      }
+    }
+    $('summonerData').attr('leaguePoints', lp)
+    callback(lp, id)
+  }
+});
+}
+
+function getSummonerStats(lp, sum_id) {
+  $.ajax({
+    url: REQUEST_URL_SHORT + '/v1.3/stats/by-summoner/' + sum_id + '/ranked' + '?api_key=' + API_KEY,
     type: 'GET',
     dataType: 'json',
     data: {},
-    success: function (json) {
-      getMostPlayedChamp(json, function(champ) {console.log(champ)});
-      getWinRate(json);
+    success: function (data) {
+      getMostPlayedChamp(lp, data);
     }
   });
 }
 
-function getMostPlayedChamp(json, callback) {
+function getMostPlayedChamp(lp, json) {
   var count = 0;
   var mpc = 0;
+  var average = 0;
   for (var i = 0; i < json['champions'].length; i++) {
-    var currentChamp = json['champions'][i]['id']
-    if (json['champions'][i]['stats']['totalSessionsPlayed'] > count && currentChamp !== 0) {
+    var currentChamp = json['champions'][i]
+    if (json['champions'][i]['stats']['totalSessionsPlayed'] > count && currentChamp['id'] !== 0) {
       mpc = currentChamp;
       count = json['champions'][i]['stats']['totalSessionsPlayed'];
     }
+    else if (currentChamp['id'] == 0) {
+      average = currentChamp;
+    }
   }
-  getChampionById(mpc, callback)
+  analyzeWins(lp, mpc, average)
 }
 
-function getWinRate(summonerID) {
-  
+function analyzeWins(lp, mpc, average) {
+  var mpc_win_rate = mpc['stats']['totalSessionsWon'] / mpc['stats']['totalSessionsPlayed'];
+  var average_win_rate = average['stats']['totalSessionsWon'] / average['stats']['totalSessionsPlayed'];
+  toProm = toPromotion(mpc_win_rate, lp)
+  //TODO API calls
 }
 
 function getChampionById(champId, callback) {
