b2f1cb5ba65b843d472c49f5183502f3112746f6
duml <Lili@dhcp-107-144.palace-wlan.simmons.edu>
2015-09-26 14:04:40 -0400

    changed params

--- js/toPromotion.js
+++ js/toPromotion.js
@@ -1,4 +1,4 @@
-function toPromotion() {
+function toPromotion(winRate winsToPromo) {
   var lossRate = 1 - winRate;
   var gamesToPromo = Math.ceil((winsToPromo / winRate) + (.5 * (winsToPromo * lossRate)));
 }
