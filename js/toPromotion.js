3def3c938d60d4d1020c26930be4ce0c8e8911a7
duml <Lili@dhcp-107-144.palace-wlan.simmons.edu>
2015-09-26 14:40:39 -0400

    mock data for currentLP

--- js/toPromotion.js
+++ js/toPromotion.js
@@ -1,5 +1,5 @@
 function toPromotion(winRate, currentLP) {
-  var winsToPromo = Math.ceil((100 - currentLP) / 20);
+  var winsToPromo = Math.ceil((100 - 73) / 20);
   var lossRate = 1 - winRate;
   return Math.ceil((winsToPromo / winRate) + (.5 * (winsToPromo * lossRate)));
 }
