a1165501bf537170af4126612b335b0c65b461ab
duml <Lili@dhcp-107-144.palace-wlan.simmons.edu>
2015-09-26 14:38:30 -0400

    comma again sorry

--- js/toPromotion.js
+++ js/toPromotion.js
@@ -1,4 +1,4 @@
-function toPromotion(winRate currentLP) {
+function toPromotion(winRate, currentLP) {
   var winsToPromo = Math.ceil((100 - currentLP) / 20);
   var lossRate = 1 - winRate;
   Math.ceil((winsToPromo / winRate) + (.5 * (winsToPromo * lossRate)));
