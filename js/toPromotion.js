af5d06284809de2555afd4db7a1df083a2c5344c
duml <Lili@dhcp-107-144.palace-wlan.simmons.edu>
2015-09-26 14:39:13 -0400

    .

--- js/toPromotion.js
+++ js/toPromotion.js
@@ -1,5 +1,5 @@
 function toPromotion(winRate, currentLP) {
   var winsToPromo = Math.ceil((100 - currentLP) / 20);
   var lossRate = 1 - winRate;
-  Math.ceil((winsToPromo / winRate) + (.5 * (winsToPromo * lossRate)));
+  return Math.ceil((winsToPromo / winRate) + (.5 * (winsToPromo * lossRate)));
 }
