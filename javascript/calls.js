b77122df717e978cb7a4d8d42a2729ee1cfe0f53
aalder <alder.a@husky.neu.edu>
2015-09-26 13:19:10 -0400

    adding variable names

---
+++ javascript/calls.js
@@ -0,0 +1,19 @@
+function serveTheMath(LP, mpcwr, awr) {
+  //serve the stats for Most Played Champion
+
+
+  //serve the stats for Average
+
+
+  //send output to the front end in a manageable way
+  document.getElementById("summonerName").innerHTML = "TODO";
+  document.getElementById("rank").innerHTML = "TODO";
+  document.getElementById("lp").innerHTML = "TODO";
+  document.getElementById("awr").innerHTML = "TODO";
+  document.getElementById("mostPlayedChamp").innerHTML = "TODO";
+  document.getElementById("mostPlayedWinRate").innerHTML = "TODO";
+  document.getElementById("gamesAverage").innerHTML = "TODO";
+  document.getElementById("gamesMostPlayed").innerHTML = "TODO";
+}
+
+//directly set variables into Alice's style
