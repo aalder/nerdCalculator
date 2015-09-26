b8290666077bfde22532ebc9bd248e7e98c85bd6
duml <Lili@dhcp-107-144.palace-wlan.simmons.edu>
2015-09-26 14:05:43 -0400

    made it a bit more simple in output

--- js/toPromotion.js
+++ js/toPromotion.js
@@ -1,4 +1,4 @@
 function toPromotion(winRate winsToPromo) {
   var lossRate = 1 - winRate;
-  var gamesToPromo = Math.ceil((winsToPromo / winRate) + (.5 * (winsToPromo * lossRate)));
+  Math.ceil((winsToPromo / winRate) + (.5 * (winsToPromo * lossRate)));
 }
