f83eef6c2f8d1e8d9e99715cbb19a105f687979a
aalder <alder.a@husky.neu.edu>
2015-09-26 13:51:54 -0400

    connetor created

--- javascript/calls.js
+++ javascript/calls.js
@@ -1,19 +1,20 @@
-function serveTheMath(LP, mpcwr, awr) {
+function serveTheMath(summonerName, rank, lp, mpc, mpcwr, awr, g2p) {
+  //Use: toPromotion(winrate, targetWins2Prom)
   //serve the stats for Most Played Champion
-
+  mpcs = toPromotion(mpcwr, g2p);
 
   //serve the stats for Average
-
+  as = toPromotion(awr, g2p);
 
   //send output to the front end in a manageable way
-  document.getElementById("summonerName").innerHTML = "TODO";
-  document.getElementById("rank").innerHTML = "TODO";
-  document.getElementById("lp").innerHTML = "TODO";
-  document.getElementById("awr").innerHTML = "TODO";
-  document.getElementById("mostPlayedChamp").innerHTML = "TODO";
-  document.getElementById("mostPlayedWinRate").innerHTML = "TODO";
-  document.getElementById("gamesAverage").innerHTML = "TODO";
-  document.getElementById("gamesMostPlayed").innerHTML = "TODO";
+  document.getElementById("summonerName").innerHTML = summonerName;
+  document.getElementById("rank").innerHTML = rank;
+  document.getElementById("lp").innerHTML = lp;
+  document.getElementById("awr").innerHTML = awr;
+  document.getElementById("mostPlayedChamp").innerHTML = mpc;
+  document.getElementById("mostPlayedWinRate").innerHTML = mpcwr;
+  document.getElementById("gamesAverage").innerHTML = as;
+  document.getElementById("gamesMostPlayed").innerHTML = mpcs;
 }
 
 //directly set variables into Alice's style
